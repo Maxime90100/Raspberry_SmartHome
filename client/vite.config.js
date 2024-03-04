dfc18ef2237f10af07a43c3576c4d0776e50349d
maxime <maxime.theveneau@gmail.com>
2024-03-04 22:22:06 +0100

    update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dotenv from 'dotenv'
 
-// https://vitejs.dev/config/
+dotenv.config()
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: process.env.VITE_HOST,
+    port: process.env.VITE_PORT,
+  },
 })
